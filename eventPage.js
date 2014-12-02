22d86f1d205ea64120e0011c0a56466880a99293
Daniel Harris <dh.harris183@gmail.com>
2014-12-02 02:16:29 -0800

    Version 1.0

--- eventPage.js
+++ eventPage.js
@@ -1,30 +1,27 @@
-//on youtube, log youtube.
-//register on youtube
 
 var getSong = function(){
   var video = document.title;
-  var song = video.replace(/(\(.*\))*(\[.*\])*( - YouTube$)/,'');
-  console.log("WE ARE WATCHING: " + song);
+  var song = video.replace(/(\(.*\))*(\[.*\])*( - YouTube$)/,'').split(' ').join('+');
+  $.get(
+    "https://api.spotify.com/v1/search?q="+ song + "&type=track", function(data){
+      var songUri = data.tracks.items[0].uri
+      if(songUri){
+        addButton();
+        $("#spotifyLink").attr("href", songUri);
+      }
+  })
 };
 
 var addButton = function(){
   var img = chrome.extension.getURL("spotifylogo.png")
-  var button = ('<button class="yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-has-icon no-icon-markup yt-uix-tooltip" type="button" title="spotify" data-tooltip-text="Play On Spotify"><img id="spotify-button-logo" src='+ img +'><span class="yt-uix-button-content">Spotify</span></button>')
-  $(button).appendTo("#watch8-secondary-actions")
+  var button = ('<a id="spotifyLink" href=""><button class="yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-has-icon no-icon-markup yt-uix-tooltip my-button" type="button" title="spotify" data-tooltip-text="Play On Spotify"><img id="spotify-button-logo" src='+ img +'><span class="yt-uix-button-content">Spotify</span></button></a>')
+  $(button).appendTo("#watch8-secondary-actions");
+  $(".ytp-button-pause").trigger("click");
 };
 
-// var getVideo = function(o){
-//   chrome.tabs.executeScript(
-//   { 
-//     code: "document.title"
-//   }, function(result){
-//     getSong(result[0]);
-//   });
-// }
 function afterNavigate() {
     if (location.pathname === '/watch') {
       getSong();
-      addButton();
     }
 };
 
