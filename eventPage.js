3a354b730849b062c591a5ef53128e1c435ca75c
Daniel Harris <dh.harris183@gmail.com>
2014-12-10 21:37:08 -0800

    Fixed a couple usability bugs

--- eventPage.js
+++ eventPage.js
@@ -1,22 +1,23 @@
 
 var getSong = function(){
   var video = document.title;
-  var song = video.replace(/(\(.*\))|(\[.*\])|( - YouTube$)/g,'').split(' ').join('+');
+  var song = video.replace(/(\(.*\))|\;|\/|\?|\:|\@|\=|\+|\$|\,|\'|(\[.*\])|( - YouTube$)/g,'').split(' ').join('+').replace(/\&/g,'and');
   $.get(
     "https://api.spotify.com/v1/search?q="+ song + "&type=track", function(data){
       var songUri = data.tracks.items[0].uri
       if(songUri){
-        addButton();
-        $("#spotifyLink").attr("href", songUri);
+        addButton(songUri);
       }
   })
 };
 
-var addButton = function(){
+var addButton = function(songUri){
   var img = chrome.extension.getURL("spotifylogo.png")
-  var button = ('<a id="spotifyLink" href=""><button class="yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-has-icon no-icon-markup yt-uix-tooltip my-button" type="button" title="spotify" data-tooltip-text="Play On Spotify"><img id="spotify-button-logo" src='+ img +'><span class="yt-uix-button-content">Spotify</span></button></a>')
+  var button = ('<a id="spotifyLink" href="'+ songUri + '"><button class="yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-has-icon no-icon-markup yt-uix-tooltip" type="button" title="spotify" data-tooltip-text="Play On Spotify"><img id="spotify-button-logo" src='+ img +'><span class="yt-uix-button-content">Spotify</span></button></a>')
   $(button).appendTo("#watch8-secondary-actions");
-  $(".ytp-button-pause").trigger("click");
+  $('#spotifyLink').on("click", function(event){
+    $(".ytp-button-pause").trigger("click");
+  });
 };
 
 function afterNavigate() {
